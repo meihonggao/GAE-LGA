2c7531c017df94e66fed94f83c305fda6618fee0
meihonggao <31908122+meihonggao@users.noreply.github.com>
2022-08-31 09:25:22 +0800

    Add files via upload

---
+++ Datasets/Dataset1/process.R
@@ -0,0 +1,27 @@
+###################################################################################################
+#思路：	对LG矩阵进行筛选获得具有组学特征的LG矩阵
+		#1.读入lnc-gene-net文件
+		#2.读入lnc和pcg的name文件
+		#3.根据name筛选获得具有组学特征的LG矩阵
+		#4.输出具有组学特征的LG矩阵
+###################################################################################################
+
+
+
+lg_path_name="/mnt/5468d/gaomeihong/LncRNA3/GAE_LGA/Datasets/Dataset3/lnc_pcg_net.txt"
+LG=read.table(file = lg_path_name,
+					sep = "\t", header = TRUE, 
+					row.names = 1, stringsAsFactors = FALSE)
+					
+lnc_path_name="/mnt/5468d/gaomeihong/LncRNA3/GAE_LGA/Datasets/Dataset3/lnc_name.txt"
+lnc_names=rownames(read.table(file = lnc_path_name,
+					sep = "\t", header = FALSE, 
+					row.names = 1, stringsAsFactors = FALSE))
+					
+pcg_path_name="/mnt/5468d/gaomeihong/LncRNA3/GAE_LGA/Datasets/Dataset3/pcg_name.txt"
+pcg_names=rownames(read.table(file = pcg_path_name,
+					sep = "\t", header = FALSE, 
+					row.names = 1, stringsAsFactors = FALSE))
+					
+LG2=LG[which(rownames(LG) %in% lnc_names),which(colnames(LG) %in% pcg_names)]
+write.table(LG2, file ="lnc_pcg_net.csv",sep=",",row.names =FALSE, col.names =FALSE, quote =TRUE);
